88136e36c1702110aca6d380522cfc9189f8cbcd
Сергей Титов <example@mail.ru>
2023-06-20 16:06:36 +0300

    ver0

---
+++ summer.py.py
@@ -0,0 +1,29 @@
+
+
+# def str_counter(my_string):
+#     my_set = set(my_string)
+#     print(my_set)
+#     for i in my_set:
+#         count = 0
+#         for j in my_string:
+#             if i == j:
+#                 count += 1
+#         print(i, count)
+
+def str_counter(my_string):
+    my_dict = {}
+    for i in my_string:
+        my_dict[i] = my_dict.get('a', 0) + 1
+    print(my_dict)
+
+
+print()
+
+print()
+
+
+
+
+
+# str_counter('abc')
+str_counter('aaaaaaaaaaaabbbbbbb454c')
