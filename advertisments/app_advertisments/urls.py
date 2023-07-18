6012c5b3d197e2667ea5e831e8bf57d5f27454d4
Сергей Титов <example@mail.ru>
2023-07-18 18:50:58 +0300

    added homework

--- advertisments/app_advertisments/urls.py
+++ advertisments/app_advertisments/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers
 urlpatterns = [
-    path('info', index),
+    path('', index, name='main_page'),
+    path('top_sellers/', top_sellers, name='top_sellers'),
 ]
+
+
+
